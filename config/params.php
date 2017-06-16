24ea166a3eae8e417a3d14ac4fe9eac78ae547c7
Alejandro Lopez <lopalejandro@gmail.com>
2017-06-16 13:25:16 -0300

    - Cambiado los colores de fondo y la pantalla de inicio

--- config/params.php
+++ config/params.php
@@ -2,4 +2,15 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+//    'components' => [
+//        'view' => [
+//            'theme' => [
+//                'basePath' => '@app/themes/orange',
+//                'baseUrl' => '@web/themes/orange',
+//                'pathMap' => [
+//                    '@app/views' => '@app/themes/orange',
+//                ],
+//            ],
+//        ],
+//    ],
 ];
